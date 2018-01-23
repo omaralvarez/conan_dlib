f3f4f25b0423a5728e29e18e696073bb01738b64
Omar Alvarez <omar.alvarez@cinfo.es>
2018-01-23 18:12:06 +0100

    Add env variable to build.py so Travis works

--- build.py
+++ build.py
@@ -3,5 +3,6 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
+    builder.password = os.getenv("CONAN_PASSWORD")
     builder.add_common_builds(shared_option_name="dlib:shared")
     builder.run()
