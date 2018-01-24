3eadf2ae1ec264c68e1939e695d73c3e34abf5ae
Omar Alvarez <omar.alvarez@cinfo.es>
2018-01-24 10:58:10 +0100

    Another try

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ class DlibConan(ConanFile):
     description = "Dlib is a modern C++ toolkit containing machine learning algorithms and tools for creating complex software in C++ to solve real world problems. See http://dlib.net for the main project documentation and API reference."
     settings = "os", "compiler", "build_type", "arch"
     options = {"iso_cpp_only" : [True, False], "enable_gif" : [True, False], "enable_png" : [True, False], "enable_jpeg" : [True, False], "no_gui_support" : [True, False], "enable_stack_trace" : [True, False], "link_with_sqlite" : [True, False], "enable_asserts" : [True, False], "enable_cuda" : [True, False], "enable_sse2" : [True, False], "enable_sse4" : [True, False], "enable_avx" : [True, False], "shared": [True, False]}
-    default_options = "iso_cpp_only=False", "enable_gif=True", "enable_png=True", "enable_jpeg=True", "no_gui_support=False", "enable_stack_trace=False", "link_with_sqlite=True", "enable_asserts=False", "enable_cuda=False", "enable_avx=False", "enable_sse4=False", "enable_sse2=True", "shared=False"
+    default_options = "iso_cpp_only=False", "enable_gif=True", "enable_png=True", "enable_jpeg=True", "no_gui_support=True", "enable_stack_trace=False", "link_with_sqlite=True", "enable_asserts=False", "enable_cuda=False", "enable_avx=False", "enable_sse4=False", "enable_sse2=True", "shared=False"
     generators = "cmake"
 
     def source(self):
