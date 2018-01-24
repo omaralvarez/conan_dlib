33a368d6721a90fbd0d5096d2c9115173b678354
Omar Alvarez <omar.alvarez@cinfo.es>
2018-01-24 13:28:41 +0100

    Test for older GCCs

--- build.py
+++ build.py
@@ -3,7 +3,6 @@ from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(args="--build missing", remotes="https://api.bintray.com/conan/bincrafters/public-conan")
-    builder.add(settings={ "compiler.libcxx": "libstdc++11" })
+    builder = ConanMultiPackager(args="--build missing -s compiler.libcxx=libstdc++11", remotes="https://api.bintray.com/conan/bincrafters/public-conan")
     builder.add_common_builds(shared_option_name="dlib:shared", pure_c=False)
     builder.run()
