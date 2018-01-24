4b0a9fdb357a18329203881ae62c7d79ab2f4bc9
Omar Alvarez <omar.alvarez@cinfo.es>
2018-01-24 10:00:17 +0100

    Since the last one was not...

--- build.py
+++ build.py
@@ -3,7 +3,7 @@ from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(remotes="https://api.bintray.com/conan/bincrafters/public-conan")
+    builder = ConanMultiPackager(args=['--build', 'missing'], remotes="https://api.bintray.com/conan/bincrafters/public-conan")
     #builder.password = os.getenv("CONAN_PASSWORD")
     builder.add_common_builds(shared_option_name="dlib:shared")
     builder.run()
