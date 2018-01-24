4b8caf7db3b061c08642749f263e32f97aca11e2
Omar Alvarez <omar.alvarez@cinfo.es>
2018-01-24 12:36:05 +0100

    Test setting for older GCC compilers

--- build.py
+++ build.py
@@ -4,6 +4,6 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(args="--build missing", remotes="https://api.bintray.com/conan/bincrafters/public-conan")
-    #builder.password = os.getenv("CONAN_PASSWORD")
+    builder.add(settings={ "compiler.libcxx": "libstdc++11" })
     builder.add_common_builds(shared_option_name="dlib:shared", pure_c=False)
     builder.run()
