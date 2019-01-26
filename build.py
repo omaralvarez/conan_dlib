363e083583d502c5d593211dc891e76d413040ff
A-Alaa <asem_alla@msn.com>
2019-01-26 02:55:51 +0200

    for version 19.16

--- build.py
+++ build.py
@@ -3,6 +3,6 @@ from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(args="--build missing -s compiler.libcxx=libstdc++11", remotes="https://api.bintray.com/conan/bincrafters/public-conan")
+    builder = ConanMultiPackager(args="--build missing -s compiler.libcxx=libstdc++11", remotes="https://api.bintray.com/a-alaa/public-conan/dlib")
     builder.add_common_builds(shared_option_name="dlib:shared", pure_c=False)
     builder.run()
