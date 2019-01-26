affddd63faf6fd20482e5710dd50fd22e66efecd
A-Alaa <asem_alla@msn.com>
2019-01-26 06:45:46 +0200

    done testing locally

--- build.py
+++ build.py
@@ -5,7 +5,7 @@ from conan.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager(
         build_policy = "missing" , 
-        remotes="https://api.bintray.com/a-alaa/public-conan/dlib")
+        remotes="https://api.bintray.com/conan/bincrafters/public-conan")
 
     builder.add({"compiler.libcxx": "libstdc++11"})
     builder.add_common_builds(shared_option_name="dlib:shared", pure_c=False)
