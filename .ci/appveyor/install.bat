e2d3036ec9ddb5026b637d08fb252a0d6d094a01
A-Alaa <asem_alla@msn.com>
2019-01-26 04:01:31 +0200

    add bincrafters package tool
    add conan user
    
    anonymize username
    
    usernames
    
    usernames
    
    update run script due to documentation
    
    login
    
    remove hyphen
    
    Add bincrafter packages
    
    build_policy argument in the constructor
    
    remove unnecessary lines in the install script
    
    build.py script
    
    update version
    
    done testing locally
    
    remove gcc4-9
    
    CI
    
    add username
    
    update README

---
+++ .ci/appveyor/install.bat
@@ -0,0 +1,5 @@
+set PATH=%PATH%;%PYTHON%/Scripts/
+pip.exe install pip --upgrade
+pip.exe install conan --upgrade
+pip.exe install conan_package_tools
+conan user a-alaa # It creates the conan data directory
