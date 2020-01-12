147d62d88858b6ba5d564d21ca09917f4cd99fa0
Michał Walenciak <Kicer86@gmail.com>
2020-01-12 08:32:45 +0100

    support for fPIC option

--- conanfile.py
+++ conanfile.py
@@ -8,10 +8,10 @@ class DlibConan(ConanFile):
     url = "https://github.com/a-alaa/conan_dlib"
     description = "Dlib is a modern C++ toolkit containing machine learning algorithms and tools for creating complex software in C++ to solve real world problems. See http://dlib.net for the main project documentation and API reference."
     settings = "os", "compiler", "build_type", "arch"
-    options = {"iso_cpp_only" : [True, False], "enable_gif" : [True, False], "enable_png" : [True, False], "enable_jpeg" : [True, False], "no_gui_support" : [True, False], "enable_stack_trace" : [True, False], "link_with_sqlite" : [True, False], "enable_asserts" : [True, False], "enable_cuda" : [True, False], "enable_blas" : [True, False], "enable_lapack" : [True, False], "enable_sse2" : [True, False], "enable_sse4" : [True, False], "enable_avx" : [True, False], "enable_mkl_fft": [True, False], "shared": [True, False]}
-    default_options = "iso_cpp_only=False", "enable_gif=True", "enable_png=True", "enable_jpeg=True", "no_gui_support=True", "enable_stack_trace=False", "link_with_sqlite=True", "enable_asserts=False", "enable_cuda=False", "enable_blas=False", "enable_lapack=False", "enable_avx=False", "enable_sse4=False", "enable_sse2=True", "enable_mkl_fft=False", "shared=False"
+    options = {"iso_cpp_only" : [True, False], "enable_gif" : [True, False], "enable_png" : [True, False], "enable_jpeg" : [True, False], "no_gui_support" : [True, False], "enable_stack_trace" : [True, False], "link_with_sqlite" : [True, False], "enable_asserts" : [True, False], "enable_cuda" : [True, False], "enable_blas" : [True, False], "enable_lapack" : [True, False], "enable_sse2" : [True, False], "enable_sse4" : [True, False], "enable_avx" : [True, False], "enable_mkl_fft": [True, False], "shared": [True, False], "fPIC": [True, False]}
+    default_options = "iso_cpp_only=False", "enable_gif=True", "enable_png=True", "enable_jpeg=True", "no_gui_support=True", "enable_stack_trace=False", "link_with_sqlite=True", "enable_asserts=False", "enable_cuda=False", "enable_blas=False", "enable_lapack=False", "enable_avx=False", "enable_sse4=False", "enable_sse2=True", "enable_mkl_fft=False", "shared=False", "fPIC=True"
     generators = "cmake"
-    
+
     def source(self):
         self.run("git clone --branch v" + self.version + " --depth 1 https://github.com/davisking/dlib.git")
         tools.replace_in_file("dlib/dlib/CMakeLists.txt", 'project(dlib)', '''project(dlib)
@@ -72,8 +72,8 @@ conan_basic_setup()
 
     def package_info(self):
         self.output.info("Compiler: %s %s" % (self.settings.compiler, self.settings.compiler.version))
-        self.output.info("Arch: %s" % self.settings.arch)      
-        self.output.info("Build_type: %s" % self.settings.build_type)     
+        self.output.info("Arch: %s" % self.settings.arch)
+        self.output.info("Build_type: %s" % self.settings.build_type)
         if self.settings.compiler == "Visual Studio":
             self.output.info("Runtime: %s" % self.settings.compiler.runtime)
         self.cpp_info.libs = ["dlib"]
